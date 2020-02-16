11bdeb33e588373e9f31d99bf0bda6b904c6eea9
E Bentley <ebentley@Es-MacBook-Air.local>
2020-02-15 18:57:08 -0600

    Add homework

---
+++ SQL Alchemy Homework/app.py
@@ -0,0 +1,54 @@
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, func
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
+### Explore Database
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def climate_analysis():
+    return (f"/api/v1.0/precipitation<br/>"
+    f"/api/v1.0/stations<br/>"
+    f"/api/v1.0/tobs<br/>"
+    f"/api/v1.0/<start><br/>"
+    f"/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/precipitation")
+def prcp_analysis():
+    last12mos = dt.date(2017,8,23)- dt.timedelta(days=365)
+    result = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last12mos).all()
+    prcp_dict = {}
+    for data in result: 
+       prcp_dict[data [0]]= data[1]
+    return jsonify(prcp_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    station = session.query(Station.station).all()
+    station_list = list(np.ravel(station))
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs_analysis():
+    tobs12mos = dt.date(2017,8,23)- dt.timedelta(days=365)
+    result = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= tobs12mos).all()
+    tobs_dict = {}
+    for data in result: 
+       tobs_dict[data [0]]= data[1]
+    return jsonify(tobs_dict)
+    
+if __name__ == "__main__":
+    app.run(port=5500)
